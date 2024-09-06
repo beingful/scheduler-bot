bf2bde6652681eaf8fdda92afc8ac04331899822
DESKTOP-KANVPQ5\hanna <hanna.kasai002@gmail.com>
2024-09-05 19:14:52 -0700

    Remove the url verification endpoint

--- Scheduler.Bot/Slack.Bot.Api/Endpoints/SlackEndpoints.cs
+++ Scheduler.Bot/Slack.Bot.Api/Endpoints/SlackEndpoints.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using SlackNet.AspNetCore;
-using SlackNet.Events;
-using System.Net;
+﻿using SlackNet.AspNetCore;
 
 namespace Slack.Bot.Api.Endpoints;
 
@@ -10,27 +7,7 @@ internal static class SlackEndpoints
     public static WebApplication AddSlackEndpoints(this WebApplication webApp)
     {
         return webApp
-            .PostUrlVerificationEndpoint()
             .PostEventEndpoint();
-    }
-
-    private static WebApplication PostUrlVerificationEndpoint(this WebApplication webApp)
-    {
-        webApp
-            .MapPost("slack/challenge", (
-                [FromBody]UrlVerification request,
-                HttpContext httpContext,
-                ISlackRequestHandler requestHandler) =>
-            {
-                return Results.Text(
-                    content: request.Challenge,
-                    contentType: "text/plain",
-                    statusCode: (int)HttpStatusCode.OK);
-            })
-            .WithDisplayName("Url verification endpoint")
-            .WithDescription("The endpoint of the Scheduler Slack Bot. Designed to verify the bot API.");
-
-        return webApp;
     }
 
     private static WebApplication PostEventEndpoint(this WebApplication webApp)
